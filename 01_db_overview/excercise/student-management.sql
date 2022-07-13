60ed4e654b2ca761d40d4a5cbeda7aa71c8473b5
huyngqa <104546437+huyngqa@users.noreply.github.com>
2022-07-13 10:20:15 +0700

    ss1

--- 01_db_overview/excercise/student-management.sql
+++ 01_db_overview/excercise/student-management.sql
@@ -1,17 +1,18 @@
-create database `student-management`;
-create table `student-management`.`student` (
-  `id` int not null primary key,
-  `name` varchar(4) null,
-  `age` int null,
-  `country` varchar(45)
+create database student_management;
+use student_management;
+create table student (
+  id_student int not null primary key,
+  name_student varchar(4) null,
+  age int null,
+  country varchar(45)
 );
-create table `student-management`.`teacher` (
-  `id` int not null primary key,
-  `name` varchar(4) null,
-  `age` int null,
-  `country` varchar(45)
+create table teacher (
+  id_teacher int not null primary key,
+  name_teacher varchar(4) null,
+  age int null,
+  country varchar(45)
 );
-create table `student-management`.`class` (
-  `id` int not null primary key,
-  `name` varchar(45) null
+create table class (
+  id_class int not null primary key,
+  name_class varchar(45) null
 );
