bab02af78b72ae56deb4beb9d0492000f79ccf37
huyngqa <104546437+huyngqa@users.noreply.github.com>
2022-07-15 17:09:36 +0700

    ss3

--- 03_manipulcation_db/excercise/manipulation_db_quan_ly_ban_hang.sql
+++ 03_manipulcation_db/excercise/manipulation_db_quan_ly_ban_hang.sql
@@ -2,29 +2,29 @@ USE quan_ly_ban_hang;
 
 INSERT INTO khach_hang
 VALUES (1,'Minh Quan',10),
-(2,'Ngoc Oanh',20),
-(3,'Hong Ha',50);
+	   (2,'Ngoc Oanh',20),
+	   (3,'Hong Ha',50);
 
 INSERT INTO don_dat_hang
 VALUES (1,1,'2006-03-21',NULL),
-(2,2,'2006-03-23',NULL),
-(3,1,'2006-03-16',NULL);
+	   (2,2,'2006-03-23',NULL),
+	   (3,1,'2006-03-16',NULL);
 
 INSERT INTO san_pham
 VALUES (1,'May Giat',3),
-(2,'Tu Lanh',5),
-(3,'Dieu Hoa',7),
-(4,'Quat',1),
-(5,'Bep Dien',2);
+	   (2,'Tu Lanh',5),
+	   (3,'Dieu Hoa',7),
+	   (4,'Quat',1),
+	   (5,'Bep Dien',2);
 
 INSERT INTO chi_tiet_hoa_don (ma_dat_hang, ma_san_pham, so_luong)
 VALUES (1,1,3),
-(1,3,7),
-(1,4,2),
-(2,1,1),
-(3,1,8),
-(2,5,4),
-(2,3,3);
+	   (1,3,7),
+	   (1,4,2),
+	   (2,1,1),
+	   (3,1,8),
+	   (2,5,4),
+	   (2,3,3);
 
 -- Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
 SELECT 
