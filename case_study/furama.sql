6f2016687c43a6b0a70dbb4dc4cd133db95b3ea2
huyngqa <104546437+huyngqa@users.noreply.github.com>
2022-07-20 09:53:28 +0700

    case-study

--- case_study/furama.sql
+++ case_study/furama.sql
@@ -510,13 +510,89 @@ WHERE
             YEAR(ngay_lam_hop_dong) IN (2019 , 2020, 2021)) AS temp);
 SET sql_safe_updates = 1;
 
--- 17. Cập nhật thông tin những khách hàng có ten_loai_khach từ Platinum lên Diamond, chỉ cập nhật những khách hàng đã từng đặt phòng với
+-- 17. Cập nhật thông tin những khách hàng có ten_loai_khach từ Platinium lên Diamond, chỉ cập nhật những khách hàng đã từng đặt phòng với
 -- Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ.
-select 
+UPDATE khach_hang 
+SET 
+    ma_loai_khach = (SELECT 
+            ma_loai_khach
+        FROM
+            loai_khach
+        WHERE
+            ten_loai_khach = 'Diamond')
+WHERE
+    ma_khach_hang IN (SELECT 
+            tmp.ma_khach_hang
+        FROM
+            (SELECT 
+                kh.ma_khach_hang
+            FROM
+                loai_khach lk
+            JOIN khach_hang kh ON lk.ma_loai_khach = kh.ma_loai_khach
+            JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+            LEFT JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+            LEFT JOIN dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+            LEFT JOIN dich_vu dv ON hd.ma_dich_vu = dv.ma_dich_vu
+            WHERE
+                (lk.ten_loai_khach = 'Platinium'
+                    AND (dv.chi_phi_thue + COALESCE(hdct.so_luong * dvdk.gia, 0) > 1000000)
+                    AND YEAR(ngay_lam_hop_dong) = 2021)) AS tmp);
 
+-- 18. Xóa những khách hàng có hợp đồng trước năm 2021 (chú ý ràng buộc giữa các bảng).
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM khach_hang 
+WHERE
+    ma_khach_hang IN (SELECT 
+        tmp.ma_khach_hang
+    FROM
+        (SELECT 
+            kh.ma_khach_hang
+        FROM
+            khach_hang kh
+        JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+        
+        WHERE
+            YEAR(ngay_lam_hop_dong) < 2021) AS tmp);
+SET SQL_SAFE_UPDATES = 1;
 
+-- 18. Cập nhật giá cho các dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2020 lên gấp đôi.
+SET SQL_SAFE_UPDATES = 0;
+UPDATE dich_vu_di_kem 
+SET 
+    gia = gia * 2
+WHERE
+    ma_dich_vu_di_kem IN (SELECT 
+            tmp.ma_dich_vu_di_kem
+        FROM
+            (SELECT 
+                dvdk.ma_dich_vu_di_kem
+            FROM
+                dich_vu_di_kem dvdk
+            JOIN hop_dong_chi_tiet hdct ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+            JOIN hop_dong hd ON hdct.ma_hop_dong = hd.ma_hop_dong
+            WHERE
+                hdct.so_luong >= 10
+                    AND YEAR(ngay_lam_hop_dong) = 2020) AS tmp);
+SET SQL_SAFE_UPDATES = 1;
 
-
-
-
-
+-- 20. Hiển thị thông tin của tất cả các nhân viên và khách hàng có trong hệ thống, 
+-- thông tin hiển thị bao gồm id (ma_nhan_vien, ma_khach_hang), ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi.
+SELECT 
+    ma_nhan_vien AS id,
+    ho_ten,
+    email,
+    so_dien_thoai,
+    ngay_sinh,
+    dia_chi
+FROM
+    nhan_vien 
+UNION ALL 
+SELECT 
+    ma_khach_hang AS id,
+    ho_ten,
+    email,
+    so_dien_thoai,
+    ngay_sinh,
+    dia_chi
+FROM
+    khach_hang
